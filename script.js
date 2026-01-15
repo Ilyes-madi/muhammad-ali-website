abfc8c6d571b8e703b06c59b5b0647dfa764660f
ovniman <ilyesmadi0212@gmail.com>
2026-01-15 15:08:46 +0100

    Initial commit - Muhammad Ali website

---
+++ script.js
@@ -0,0 +1,13 @@
+//script JavaScript
+alert("Bienvenue sur la page dédiée à Mohamed Ali");
+
+// Déclaration de 2 variables correspondant à la base et la hauteur
+let base = 5;
+let hauteur = 15;
+
+// Calcule de l'air du triangle
+const aire = (base * hauteur/2);
+
+console.log("l'aire de ce triangle est:" + aire);
+//affichage du résultat
+console.log("JS fonctionne");
